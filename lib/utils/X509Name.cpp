c70378edb7ba50d0935ea4a475ecb8d143979ff8
Danil Kovalev <kovalevdaal@gmail.com>
2019-05-27 14:00:02 +0200

    add x509Name under object holder

--- lib/utils/X509Name.cpp
+++ lib/utils/X509Name.cpp
@@ -1,28 +1,34 @@
 #include "X509Name.h"
 
-#include <memory>
-
-#include "utils.h"
+#include "ObjectHelper.h"
 #include "SslException.h"
 
-X509Name::X509Name(X509_NAME* raw, bool acquire) noexcept
-: m_raw(raw)
-, m_acquired(acquire)
-{
-}
+#include <memory>
 
-std::string X509Name::string() const
+std::string X509Name::toString() const
 {
     return std::to_string(m_raw);
 }
 
-std::string std::to_string(const X509_NAME* raw)
+X509_NAME* X509Name::duplicate(X509_NAME* raw)
 {
-    char* name = X509_NAME_oneline(raw, nullptr, 0);
-    if (name == nullptr)
-        throw SslException("X509_NAME_oneline");
+    X509_NAME* result = X509_NAME_dup(raw);
+    if (result == nullptr)
+        throw SslException("Failed to duplicate X509 Name");
 
-    auto holder = makeObjectHolder(name);
-    return name;
+    return result;
 }
 
+void X509Name::destroy(X509_NAME* raw) noexcept
+{
+    X509_NAME_free(raw);
+}
+
+std::string std::to_string(const X509_NAME* raw)
+{
+    std::unique_ptr<char> ptrName(X509_NAME_oneline(raw, nullptr, 0));
+    if (!ptrName)
+        throw SslException("X509_NAME_oneline");
+
+    return ptrName.get();
+}
--- lib/utils/X509Name.h
+++ lib/utils/X509Name.h
@@ -1,23 +1,46 @@
 #pragma once
 
-#include <string>
+#include "ObjectHolder.h"
 
 #include <openssl/x509.h>
 
-class X509Name
-{
-public:
-    X509Name(X509_NAME* raw, bool acquire) noexcept;
+#include <string>
 
-    std::string string() const;
-private:
-    X509_NAME* m_raw;
-    bool m_acquired;
+class X509Name : public ObjectHolder<X509_NAME, X509Name>
+{
+  public:
+    X509Name(X509_NAME* raw, bool acquire) noexcept
+      : ObjectHolder(raw, acquire)
+    {
+    }
+
+  public:
+    using RawType = X509_NAME;
+
+    X509Name(const X509Name& other) = default;
+    X509Name(X509Name& other) = default;
+
+    X509Name& operator=(const X509Name& other)
+    {
+        ObjectHolder::operator=(other);
+        return *this;
+    }
+
+    X509Name& operator=(X509Name&& other) noexcept
+    {
+        ObjectHolder::operator=(std::move(other));
+        return *this;
+    }
+
+    ~X509Name() = default;
+
+    static X509_NAME* duplicate(X509_NAME* raw);
+    static void destroy(X509_NAME* raw) noexcept;
+
+    std::string toString() const;
 };
 
 namespace std
 {
-    std::string to_string(const X509_NAME* raw);
+std::string to_string(const X509_NAME* raw);
 }
-
-
